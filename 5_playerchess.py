dee662f9d8689c129d95c58fc92d9d5139885c2e
Harsha <79503095+HarshaLokesh@users.noreply.github.com>
2022-09-10 17:16:01 +0530

    Update 5_playerchess.py
    Displaying player grid (level 1)

--- 5_playerchess.py
+++ 5_playerchess.py
@@ -1,5 +1,18 @@
+# level 1
 def display_player_grid(grid):
     for player_row in grid:
         for player in player_row:
-            print(player)
+            print(player, end=" ")
+        print()
 
+
+grid = [
+['-',	'-'	,	'-'		,'-'	,	'-'], 
+['-',	'-'	,	'-'		,'-'	,	'-'],
+['-',	'-'	,	'-'		,'-'	,	'-'],
+['-',	'-'	,	'-'		,'-'	,	'-'],
+['-',	'-'	,	'-'		,'-'	,	'-']
+]
+
+
+display_player_grid(grid)
