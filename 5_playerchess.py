69d1691b9a852eb4b0a98f447376859cd000c013
Harsha <79503095+HarshaLokesh@users.noreply.github.com>
2022-09-10 17:30:33 +0530

    Update 5_playerchess.py
    Adding pawns of A and B to chess grid

--- 5_playerchess.py
+++ 5_playerchess.py
@@ -14,5 +14,22 @@ grid = [
 ['-',	'-'	,	'-'		,'-'	,	'-']
 ]
 
+if __name__ == '__main__':
+    n = 5 # number of players is 5
+    print("Player1 Input: ") #input names of pawns player A
+    player_A = input()
+    player_A = player_A.split(",") #input 5 player with comma for Player A
+    
+    print("Player2 Input: ") #input names of pawns player B
+    player_B = input()
+    player_B = player_B.split(",") #input 5 player with comma for Player B
 
-display_player_grid(grid)
+    
+    #adding players to board
+    for i in range(5):   
+        grid[0][i] = player_A[i]
+    for i in range(5):
+        grid[4][i] = player_B[i]
+
+    display_player_grid(grid)
+    
